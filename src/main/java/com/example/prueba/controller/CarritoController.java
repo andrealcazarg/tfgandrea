3a6a258389ed25170721bd32d2360ebc30178c0d
andrea <andreaag929@gmail.com>
2022-05-03 19:00:03 +0200

    fronted

--- src/main/java/com/example/prueba/controller/CarritoController.java
+++ src/main/java/com/example/prueba/controller/CarritoController.java
@@ -1,10 +1,12 @@
 package com.example.prueba.controller;
 
+import com.example.prueba.model.Producto;
 import com.example.prueba.services.producto.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CarritoController {
@@ -18,5 +20,11 @@ public class CarritoController {
     public String listarUsu(Model model){
         model.addAttribute("listaProductosTienda", servicio.findAll()) ; // inyecta el servicio gracias al @Autowired anterior
         return "tienda";
+    }
+
+    @GetMapping({"/carrito/list/{idProducto}"})
+    public String listarDescripcion(Model model, @PathVariable Producto producto){
+        model.addAttribute("listaProductosTienda", servicio.findAll()) ; // inyecta el servicio gracias al @Autowired anterior
+        return "productos";
     }
 }
