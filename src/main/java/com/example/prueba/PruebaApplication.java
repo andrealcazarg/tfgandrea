77b86c5e48cd14567e1624566caf059581b898ed
andreaag929@gmail.com <Ann20covid>
2022-05-29 22:05:08 +0200

    subir al host.falla bootstrap

--- src/main/java/com/example/prueba/PruebaApplication.java
+++ src/main/java/com/example/prueba/PruebaApplication.java
@@ -2,12 +2,19 @@ package com.example.prueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PruebaApplication {
+public class PruebaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PruebaApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PruebaApplication.class);
+
+    }
 }
